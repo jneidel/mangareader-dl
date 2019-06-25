1b02d055a683042e03b7247de2400770306249d3
Jonathan Neidel <git@jneidel.com>
2019-06-25 14:17:09 +0200

    Update cli to use new parsers and functions

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,168 +1,26 @@
-#!/usr/bin/env node
-
-import * as path from "path";
-import meow from "meow";
-import { init as initializeErrHndlr } from "err-hndlr";
-
-import * as s from "../lib/settings";
-import * as log from "../lib/log" ;
-import * as cliCommands from "../lib/cli-commands" ;
-
-import * as providers from "../providers";
+import * as util from "./util";
+import { flagParser, commandParser } from "./parser";
+import { version, help } from "./flags";
 
 ( async function main() {
-  const settingsPath = await s.getSettingsPath();
-  const settings = s.createSettingsObject( settingsPath );
-  const defaults = s.parseDefaults( settings );
+  const providers = await util.getProviderList();
 
-  const cli = meow( `Usage
-  $ mangareader-dl <manga>
+  let { flags, commands } = flagParser( process.argv );
+  commands = commandParser( commands );
 
-Commands
-  <manga> Manga to download
-  list    List downloaded manga
-  config  Set defaults
-  update  Update subscribed manga
-
-Options, Sub-commands
-  <manga>
-    -o, --out       Set output path
-    -d, --dir       Download into 'path/manga-name'
-    -p, --provider  Set download site
-    -f, --force     Overwrite history
-    -s, --subscribe Subscribe to new chapters
-    -m, --micro     Micro progress bar
-  list
-    -l, --latest    Highlight if new chapters are available
-    reset           Reset non-subscribed manga
-      -f, --force   Reset history
-  config
-    -o, --out       Set default output path
-    -d, --dir       Enable/disable dir option
-    -p, --provider  Set default provider
-    reset           Reset config
-  update
-    -m, --micro     Micro progress bar
-        --silent    Hide progress bar
-    check           Check if new chapters are available
-  --version         Show version
-  --help            This help message
-  --debug           Throw errors locally
-
-Providers
-  The supported providers are:
-  ${providers.list.join( ", " )}
-
-Examples
-  $ mangareader-dl mangareader.net/naruto/100 -do .
-  => Download naruto chapter 100+ into cwd
-
-  $ mangareader-dl naruto -mp mangareader
-  => Download naruto from mangareader.net in micro mode
-
-For the full documentation please refer to:
-https://github.com/jneidel/mangareader-dl`, {
-    description: "mangareader-dl: CLI for comfortable manga download",
-    flags      : {
-      out: {
-        alias  : "o",
-        type   : "string",
-        default: defaults.out,
-      },
-      dir: {
-        alias  : "d",
-        type   : "boolean",
-        default: defaults.dir,
-      },
-      force: {
-        alias  : "f",
-        type   : "boolean",
-        default: false,
-      },
-      provider: {
-        alias  : "p",
-        type   : "string",
-        default: defaults.provider,
-      },
-      micro: {
-        alias  : "m",
-        type   : "boolean",
-        default: false,
-      },
-      subscribe: {
-        alias  : "s",
-        type   : "boolean",
-        default: false,
-      },
-      latest: {
-        alias  : "l",
-        type   : "boolean",
-        default: false,
-      },
-      silent: {
-        type   : "boolean",
-        default: false,
-      },
-      debug: {
-        type   : "boolean",
-        default: false,
-      },
-    },
-  }
-  );
-
-  // Clean up input
-  const args = cli.flags;
-  args._ = cli.input;
-
-  if ( args._.length === 0 ) { // Nothing passed
-    log.prompt( "Specify '--help' for available commands" );
+  if ( flags.version ) {
+    const versionString = await version();
+    console.log( versionString );
     process.exit();
   }
 
-  // flags require arguments
-  [ { name: "out", val: args.out }, { name: "provider", val: args.provider } ].forEach( arg => {
-    if ( arg.val === "" ) {
-      log.prompt( `The '--${arg.name}' flag requires a parameter. Specify '--help' for available commands` );
-      process.exit();
-    }
-  } );
-
-  let outputPath = path.normalize( args.out );
-  outputPath = path.isAbsolute( args.out ) ? args.out : path.resolve( process.cwd(), args.out );
-
-  if ( providers.isProvider( args.provider ) ) {
-    log.prompt( `The provider '${args.provider}' is not supported.\nSee --help for available providers.` );
+  if ( flags.help || commands.length === 0 ) {
+    const helpString = help( commands, providers );
+    console.log( helpString );
     process.exit();
-  }
-
-  args.bar = args.micro ? "micro" : "extended"; // Micro > extended
-
-  const isReset = args._[1] === "reset";
-
-  // Initialize error handler
-  const throwErrorCondition = args.debug;
-  const errorRestApi = "https://api.jneidel.com/errors/submit";
-  const errorBaseData = { id: "11fbf6a8-40ea-461f-af94-333280bb3c41" };
-  initializeErrHndlr( throwErrorCondition, errorRestApi, errorBaseData, { app: require( "../../package.json" ), os: true } );
-
-  // Parse commands
-  switch ( args._[0] ) {
-    case "list":
-      cliCommands.list( settings, isReset ? { settingsPath, force: args.force } : null, args.latest );
-      break;
-    case "config":
-      cliCommands.config( args, settings, defaults, outputPath, isReset ? settingsPath : null );
-      break;
-    case "update":
-      cliCommands.update( args, settings );
-      break;
-    default: // <manga>
-      cliCommands.manga( args, outputPath, settings );
   }
 } )();
 
-process.on( "unhandledRejection", ( err ) => {
-  log.error( "", { err }, true );
+process.on( "unhandledRejection", err => {
+  console.log( err );
 } );
-
