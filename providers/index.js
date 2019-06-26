22c3ee50d050f3e7a8feb5cf2d6a00f14782f9f0
Jonathan Neidel <jneidel@jneidel.com>
2019-06-26 23:36:10 +0200

    Remove readmng

--- providers/index.js
+++ providers/index.js
@@ -1,17 +1,14 @@
 const mangareader = require( "./mangareader" );
-const readmng = require( "./readmng" );
 const mangalife = require( "./mangalife" );
 
 exports.extensions = {
   mangareader: mangareader.extension,
   mangapanda : "com", // Reuses mangareader file, but not same extension
-  readmng    : readmng.extension,
   mangalife  : mangalife.extension,
 };
 
 exports.getLib = provider =>
   provider === "mangareader" ? mangareader :
   provider === "mangalife" ? mangalife :
-  provider === "readmng" ? readmng :
   provider === "mangapanda" ? mangareader :
   null; // Provider does not match
