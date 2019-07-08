f94515f3a9e4c5f98681c9158dcbf6ba0aa9c37a
Jonathan Neidel <jneidel@jneidel.com>
2019-07-08 23:38:40 +0200

    Add tasks to cli

--- bin/cli.ts
+++ bin/cli.ts
@@ -8,6 +8,8 @@ import { version, help } from "./flags";
   let { flags, commands } = flagParser( process.argv );
   commands = commandParser( commands );
   flags = validateFlags( flags, commands, providers )
+  // Flag validator (filter out irrelevant flags for command,
+  //  validatate passed flags to be valid for command)
 
   if ( flags.version ) {
     const versionString = await version();
@@ -20,6 +22,10 @@ import { version, help } from "./flags";
     console.log( helpString );
     process.exit();
   }
+
+  console.log( commands, flags )
+  // Command validation (has required, correct usage, correct order=
+  // Command runner (case exec commands, * as manga, dynamically create manga from provider based on given -p or url (or default))
 } )();
 
 process.on( "unhandledRejection", err => {
